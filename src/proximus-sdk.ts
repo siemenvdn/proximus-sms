1e6e08c5eec9e885af26588268fd9e09fad18770
siemenvdn <siemen.vandenneste@wisemen.digital>
2024-01-30 15:34:50 +0100

    add basic functionality

--- src/proximus-sdk.ts
+++ src/proximus-sdk.ts
@@ -3,7 +3,14 @@ import { OutboundMessagePayload } from "./types/payloads/outbound-message-payloa
 import { OutboundMessageResponse } from './types/responses/outbound-message-response.type'
 import { OutboundMessageLengthResponse } from './types/responses/outbound-message-length-response.type'
 
-export class ProximusSMSOutbound {
+interface ProximusSMSOutboundInterface {
+    send(options: OutboundMessagePayload): Promise<OutboundMessageResponse>;
+    length(options: OutboundMessagePayload): Promise<OutboundMessageLengthResponse>;
+    deliveryInfo(messageId: string): Promise<OutboundMessageResponse>;
+    sendAddress(options: OutboundMessagePayload): Promise<OutboundMessageResponse>;
+}
+
+export class ProximusSMSOutbound implements ProximusSMSOutboundInterface {
     protected readonly clientId: string
     protected readonly clientSecret: string
     protected accessToken: string | null = null
@@ -12,16 +19,17 @@ export class ProximusSMSOutbound {
     constructor (options: { clientId: string, clientSecret: string }) {
         this.clientId = options.clientId
         this.clientSecret = options.clientSecret
-        this.client = new Axios({ baseURL: 'https://api.enco.io/sms/1.0.0', timeout: 5000 })
+        this.client = new Axios({ baseURL: 'https://api.enco.io', timeout: 5000 })
     }
 
     private async getAccessToken (): Promise<string> {
         if (this.accessToken == null) {
-            const response = await this.client.post('/oauth/token', {
+            const response = await this.client.post('/token', {
                 grant_type: 'client_credentials',
+                scope: 'openid',
                 client_id: this.clientId,
-                client_secret: this.clientSecret
-            })
+                client_secret: this.clientSecret,
+            }, { headers: { 'Content-Type': 'application/x-www-form-urlencoded', 'Accept': 'application/json' } })
             this.accessToken = response['access_token']
         }
         return this.accessToken
@@ -32,32 +40,31 @@ export class ProximusSMSOutbound {
         return { headers: { Authorization: `Bearer ${accessToken}` } }
     }
 
-    public async outboundMessagesSend (options: OutboundMessagePayload): Promise<OutboundMessageResponse> {
-        const response = await this.sendRequest('/outbound-messages', options)
+    public async send (options: OutboundMessagePayload): Promise<OutboundMessageResponse> {
+        const response = await this.sendSMSRequest('/outbound-messages', options)
         return response.data
     }
 
-    public async outboundMessagesLength (options: OutboundMessagePayload): Promise<OutboundMessageLengthResponse> {
-        const response = await this.sendRequest('/outbound-messages/length', options)
+    public async length (options: OutboundMessagePayload): Promise<OutboundMessageLengthResponse> {
+        const response = await this.sendSMSRequest('/outbound-messages/length', options)
         return response.data
     }
 
-    public async outboundMessagesDeliveryInfo (messageId: string): Promise<OutboundMessageResponse> {
-        const response = await this.sendRequest(`/outbound-messages/delivery-info/${messageId}`)
+    public async deliveryInfo (messageId: string): Promise<OutboundMessageResponse> {
+        const response = await this.sendSMSRequest(`/outbound-messages/delivery-info/${messageId}`)
         return response.data as OutboundMessageResponse
     }
 
-    public async outboundMessagesAddress (options: OutboundMessagePayload): Promise<OutboundMessageResponse> {
-        const response = await this.sendRequest('/outbound-messages/address', options)
+    public async sendAddress (options: OutboundMessagePayload): Promise<OutboundMessageResponse> {
+        const response = await this.sendSMSRequest('/outbound-messages/address', options)
         return response.data
     }
 
-    private async sendRequest(baseUrl: string, options?: OutboundMessagePayload): Promise<any> {
+    private async sendSMSRequest(baseUrl: string, options?: OutboundMessagePayload): Promise<any> {
         const url = `${baseUrl}` + (options.forceCharacterLimit ? `?forceCharacterLimit=${options.forceCharacterLimit}` : '')
         const config = await this.getAxiosConfig()
-        const response = await this.client.post(url, options.body, config)
+        const response = await this.client.post('/sms/1.0.0' + url, options.body, config)
         if(response.status !== 200) throw new Error(`Return status ${response.status}, response: ${response.data}`)
-        console.log('url')
         return response.data
     }
 }
